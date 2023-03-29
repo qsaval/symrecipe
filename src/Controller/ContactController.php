7b0c7b1936ba65b874b78ca8df670a6d2b1bf891
qsaval <quentin.saval@hotmail.fr>
2023-03-29 13:52:12 +0200

    modif

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,19 +4,17 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use App\Service\MailService;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailService $mailService): Response
     {
         $contact = new Contact();
 
@@ -34,18 +32,12 @@ class ContactController extends AbstractController
             $manager->persist($contact);
             $manager->flush();
 
-            $email = (new TemplatedEmail())
-            ->from($contact->getEmail())
-            ->to('admin@symrecipe.com')
-            ->subject($contact->getSubject())
-            ->htmlTemplate('emails/contact.html.twig')
-            
-            //pass variable (name => valude) to the template
-            ->context([
-                'contact' => $contact,
-            ]);
-
-            $mailer->send($email);        
+            $mailService->sendEmail(
+                $contact->getEmail(),
+                $contact->getSubject(),
+                'emails/contact.html.twig',
+                ['contact' => $contact]
+            );
 
             $this->addFlash(
                 'success', 
