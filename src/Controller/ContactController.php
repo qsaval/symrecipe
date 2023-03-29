59616cf97f1e9c70faf19f78f125250dbfa1fc6a
qsaval <quentin.saval@hotmail.fr>
2023-03-29 12:05:04 +0200

    modif

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,15 +5,18 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
 
@@ -30,6 +33,19 @@ class ContactController extends AbstractController
 
             $manager->persist($contact);
             $manager->flush();
+
+            $email = (new TemplatedEmail())
+            ->from($contact->getEmail())
+            ->to('admin@symrecipe.com')
+            ->subject($contact->getSubject())
+            ->htmlTemplate('emails/contact.html.twig')
+            
+            //pass variable (name => valude) to the template
+            ->context([
+                'contact' => $contact,
+            ]);
+
+            $mailer->send($email);        
 
             $this->addFlash(
                 'success', 
